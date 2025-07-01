e9ea4b8c6739706f5a478088c9d8df7d15b7ca2c
Anmolzezx <anmolsingh7442@gmail.com>
2025-07-02 00:56:01 +0530

    apply my chats on flatlist

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,9 +6,12 @@ import {
   FlatList,
   TextInput,
   TouchableOpacity,
-  StyleSheet,
   NativeSyntheticEvent,
   NativeScrollEvent,
+  KeyboardAvoidingView,
+  Platform,
+  TouchableWithoutFeedback,
+  Keyboard,
 } from "react-native";
 import backArrow from "../../assets/icons/back-arrow.png";
 import edit from "@/assets/icons/edit.png";
@@ -17,6 +20,7 @@ import ChatMenu from "@/components/ThreeDotsMenu";
 import { useEffect, useRef, useState } from "react";
 import { Feather } from "@expo/vector-icons";
 import axios from "axios";
+import { list } from "postcss";
 
 export default function HomeScreen() {
   const [value, setValue] = useState("");
@@ -52,14 +56,12 @@ export default function HomeScreen() {
   };
 
   useEffect(() => {
-    fetchChats(page);
+    const getChats = async () => {
+      await fetchChats(page);
+    };
+    getChats();
   }, [page]);
 
-  // const handleScroll = ({ nativeEvent }) => {
-  //   if (nativeEvent.contentOffset.y < 50 && !loading) {
-  //     setPage((prev) => prev + 1);
-  //   }
-  // };
   const handleScroll = (event: NativeSyntheticEvent<NativeScrollEvent>) => {
     const { nativeEvent } = event;
     if (nativeEvent.contentOffset.y < 50 && !loading) {
@@ -69,21 +71,33 @@ export default function HomeScreen() {
 
   const renderItem = ({ item }: { item: Chat }) => {
     const isMe = item.sender.self;
+    const isVerified = item.sender.is_kyc_verified;
     return (
       <View
         className={`flex-row items-end my-1 ${
           isMe ? "justify-end" : "justify-start"
         }`}
       >
-        <Image
-          source={{ uri: item.sender.image }}
-          className="w-8 h-8 rounded-full mx-1"
-          resizeMode="cover"
-        />
+        {!isMe && (
+          <View className="relative w-8 h-8 mx-1">
+            <Image
+              source={{ uri: item.sender.image }}
+              className="w-8 h-8 rounded-full mx-1"
+              resizeMode="cover"
+            />
+            {isVerified && (
+              <Image
+                source={require("../../assets/icons/verified.png")}
+                className="w-4 h-4 absolute -bottom-1 -right-1"
+                resizeMode="contain"
+              />
+            )}
+          </View>
+        )}
 
         <View
-          className={`max-w-[75%] px-4 py-2 rounded-xl ${
-            isMe ? "bg-blue-500 rounded-tr-none" : "bg-gray-200 rounded-tl-none"
+          className={`max-w-[75%] px-4 py-2 font-Jakarta rounded-xl ${
+            isMe ? "bg-blue-500 rounded-tr-none" : "bg-white rounded-tl-none"
           }`}
         >
           <Text className={`${isMe ? "text-white" : "text-black"}`}>
@@ -93,88 +107,124 @@ export default function HomeScreen() {
       </View>
     );
   };
+  const handleSend = () => {
+    if (value.trim() === "") return;
+    const newMessage: Chat = {
+      id: Date.now().toString(),
+      message: value,
+      time: new Date().toISOString(),
+      sender: {
+        self: true,
+        image:
+          "https://fastly.picsum.photos/id/1072/160/160.jpg?hmac=IDpbpA5neYzFjtkdFmBDKXwgr-907ewXLa9lLk9JuA8",
+        is_kyc_verified: true,
+      },
+    };
+    setChats((prev) => [...prev, newMessage]);
+    setValue("");
+  };
   return (
-    <SafeAreaView className="flex-1 bg-gray-100">
-      <View className="w-full h-full ">
-        <View className="flex flex-row items-center w-full justify-between px-3 py-6">
-          <Image
-            source={backArrow}
-            alt="back"
-            className="h-6 w-6"
-            resizeMode="contain"
-          />
-          <Text className="flex-1 font-JakartaBold text-3xl ml-1">Trip 1</Text>
-          <Image
-            source={edit}
-            alt="back"
-            className="h-6 w-6"
-            resizeMode="contain"
-          />
-        </View>
-        <View className="flex-row items-center justify-between  px-1 py-3 mx-3 rounded-xl">
-          <View className="flex-row -space-x-2">
-            <Image
-              source={profileD}
-              className="w-6 h-6 rounded-full border=2"
-              resizeMode="cover"
-            />
-          </View>
-          <View className="flex-1 ml-4">
-            <Text className="text-2xl text-gray-500">
-              From{" "}
-              <Text className="font-semibold text-black"> IGI Airport, T3</Text>
-            </Text>
-            <Text className="text-2xl text-gray-500">
-              To <Text className="font-semibold text-black"> Sector 28</Text>
-            </Text>
-          </View>
-          <ChatMenu />
-        </View>
-
-        <View className="w-full h-[1px] bg-gray-200 mt-5"></View>
-        <FlatList
-          data={chats}
-          ref={listRef}
-          inverted
-          renderItem={renderItem}
-          onScroll={handleScroll}
-          contentContainerStyle={{ padding: 10 }}
-        />
-
-        <View className=" relative px-6 py-2 w-full">
-          <TextInput
-            placeholder="Reply to @Rohit Yadav"
-            className="rounded-lg pl-4 py-3 pr-16 w-full text-black bg-white"
-            placeholderTextColor="#888"
-            value={value}
-            onChangeText={setValue}
-          />
-          <TouchableOpacity
-            onPress={() => setShowActions(!showActions)}
-            className="absolute right-20 top-4"
-          >
-            <Feather name="paperclip" size={20} color="#333" />
-          </TouchableOpacity>
-          <TouchableOpacity className="absolute right-10 top-4">
-            <Feather name="send" size={22} color="#333" />
-          </TouchableOpacity>
-          {showActions && (
-            <View className="absolute right-12 bottom-12 bg-green-700 px-3 py-2 rounded-xl flex-row items-center space-x-4 z-10">
-              <TouchableOpacity>
-                <Feather name="camera" size={20} color="white" />
-              </TouchableOpacity>
-              <TouchableOpacity>
-                <Feather name="video" size={20} color="white" />
-              </TouchableOpacity>
-              <TouchableOpacity>
-                <Feather name="file-text" size={20} color="white" />
-              </TouchableOpacity>
-
-              <View className="absolute -bottom-1 right-6 w-0 h-0 border-1-[6px] border-r-[6px] border-b-[6px] border-1-transparent border-r-transparent border-b-green-700" />
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      keyboardVerticalOffset={Platform.OS === "ios" ? 100 : 80}
+      className="flex-1"
+    >
+      <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+        <SafeAreaView className="flex-1 bg-gray-100">
+          <View className="w-full h-full ">
+            <View className="flex flex-row items-center w-full justify-between px-3 py-6">
+              <Image
+                source={backArrow}
+                alt="back"
+                className="h-6 w-6"
+                resizeMode="contain"
+              />
+              <Text className="flex-1 font-JakartaBold text-3xl ml-1">
+                Trip 1
+              </Text>
+              <Image
+                source={edit}
+                alt="back"
+                className="h-6 w-6"
+                resizeMode="contain"
+              />
             </View>
-          )}
-        </View>
-      </View>
-    </SafeAreaView>
+            <View className="flex-row items-center justify-between  px-1 py-3 mx-3 rounded-xl">
+              <View className="flex-row -space-x-2">
+                <Image
+                  source={profileD}
+                  className="w-6 h-6 rounded-full border=2"
+                  resizeMode="cover"
+                />
+              </View>
+              <View className="flex-1 ml-4">
+                <Text className="text-2xl text-gray-500">
+                  From{" "}
+                  <Text className="font-semibold text-black">
+                    {" "}
+                    IGI Airport, T3
+                  </Text>
+                </Text>
+                <Text className="text-2xl text-gray-500">
+                  To{" "}
+                  <Text className="font-semibold text-black"> Sector 28</Text>
+                </Text>
+              </View>
+              <ChatMenu />
+            </View>
+
+            <View className="w-full h-[1px] bg-gray-200 mt-5"></View>
+            <FlatList
+              data={chats}
+              ref={listRef}
+              renderItem={renderItem}
+              onScroll={handleScroll}
+              contentContainerStyle={{ padding: 10 }}
+            />
+
+            <View className=" relative px-6 py-10 w-full">
+              <TextInput
+                placeholder="Reply to @Rohit Yadav"
+                className="rounded-lg pl-4 py-3 pr-16 w-full h-12 text-lg text-black bg-white"
+                placeholderTextColor="#888"
+                value={value}
+                onChangeText={setValue}
+              />
+              <TouchableOpacity
+                onPress={() => setShowActions(!showActions)}
+                className="absolute right-20 bottom-12"
+              >
+                <Feather name="paperclip" size={20} color="#333" />
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={handleSend}
+                className="absolute right-10 bottom-12"
+              >
+                <Feather name="send" size={22} color="#333" />
+              </TouchableOpacity>
+              {showActions && (
+                <View className="absolute right-10 bottom-24 bg-green-700 px-3 py-4 rounded-full flex-row items-center justify-between w-32 z-10">
+                  <TouchableOpacity>
+                    <Feather name="camera" size={20} color="white" />
+                  </TouchableOpacity>
+                  <TouchableOpacity>
+                    <Feather name="video" size={20} color="white" />
+                  </TouchableOpacity>
+                  <TouchableOpacity>
+                    <Feather name="file-plus" size={20} color="white" />
+                  </TouchableOpacity>
+
+                  <View
+                    className="absolute -bottom-1 right-12 w-0 h-0
+  border-l-[6px] border-r-[6px] border-t-[6px]
+  border-l-transparent border-r-transparent border-t-green-700"
+                  />
+                </View>
+              )}
+            </View>
+          </View>
+        </SafeAreaView>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
--- types/type.d.ts
+++ types/type.d.ts
@@ -7,7 +7,9 @@ type Chat = {
   id: string;
   message: string;
   sender: {
+    is_kyc_verified: boolean;
     self: boolean;
     image: string;
   };
+  time: string;
 };
